086f1e33f159a765d4ce3ad05dbf0bb4a55431cf
0xShravanth <shravanth0023@gmail.com>
2025-02-10 17:34:40 +0530

    example 1 starter file cargo

---
+++ rust-examples1/target/debug/deps/rust_examples1.d
@@ -0,0 +1,5 @@
+D:\Rust\rust-examples1\target\debug\deps\rust_examples1.exe: src\main.rs
+
+D:\Rust\rust-examples1\target\debug\deps\rust_examples1.d: src\main.rs
+
+src\main.rs:
---
+++ rust-examples1/target/debug/rust-examples1.d
@@ -0,0 +1,1 @@
+D:\Rust\rust-examples1\target\debug\rust-examples1.exe: D:\Rust\rust-examples1\src\main.rs
