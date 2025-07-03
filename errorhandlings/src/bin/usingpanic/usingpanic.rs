4be4574237659e46a0a2d105f39dc6c4da26b234
0xShravanth <shravanth0023@gmail.com>
2025-07-03 23:57:14 +0530

    changes in  errorhandling

---
+++ errorhandlings/src/bin/usingpanic/usingpanic.rs
@@ -0,0 +1,7 @@
+fn main(){
+    // This will cause a panic
+    panic!("This is a panic message");
+    
+    // The program will not reach this point
+    println!("This line will not be executed");
+}
